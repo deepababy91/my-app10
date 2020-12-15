70474a58004bd64b14a553727173b25ad6b2a9f7
Deepa Baby <deepababy91@gmail.com>
2020-12-15 00:33:10 -0600

    Updates

---
+++ precache-manifest.1654feae83d58d5cf8d5f67fa8c0358f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2dd8240be9865b49a2b06ae7c4b51a1",
+    "url": "/my-app10/index.html"
+  },
+  {
+    "revision": "8cf7611d96181f61a9d9",
+    "url": "/my-app10/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7e0f35ae2724ac90aab2",
+    "url": "/my-app10/static/js/2.c99b1d44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app10/static/js/2.c99b1d44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf7611d96181f61a9d9",
+    "url": "/my-app10/static/js/main.68216191.chunk.js"
+  },
+  {
+    "revision": "2ab8edd51dd94d6ab0a0",
+    "url": "/my-app10/static/js/runtime-main.a60ca774.js"
+  }
+]);
